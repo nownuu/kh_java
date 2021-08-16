a0b8b660186322cdaae64ead48f373e737cdf0c3
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-08-16 11:54:12 +0900

    Calculate

--- day 05/src/Calculate2.java
+++ day 05/src/Calculate2.java
@@ -1,6 +1,6 @@
 import java.lang.*;
 
-public class Calculate2 {
+public class Calculate3 {
 	public static void main (String[] args) {
 		
 		// Q : 5000원짜리 자장면 2 그릇과 6000원짜리 짬뽕 2그릇을 주문할 때의 각 메뉴의 총가격과 가격의 합계는?

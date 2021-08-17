885c820581d3e7dca4c26fd454983cb6ae6fcb68
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-08-17 15:15:53 +0900

    연산

---
+++ day 05/src/logical/Test02.java
@@ -0,0 +1,19 @@
+package logical;
+import java.lang.*;
+public class Test02 {
+	public static void main(String[] args) {
+		// 논리형 데이터 저장
+		
+		// boolean a = 10;
+		
+		boolean a = true;//false
+		
+		boolean b =50>30;
+		System.out.println(b);
+		
+		int speed = 60;
+		
+		boolean speeding = speed > 50;
+		System.out.println(speeding);
+	}
+}

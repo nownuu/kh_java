c0e09cfb110ead588cf86e366e3a3ad4a52e5cdb
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-08-17 10:33:15 +0900

    5일차 실수정수

---
+++ day 05/src/realnumber/Test01.java
@@ -0,0 +1,24 @@
+package realnumber;
+
+import java.lang.*;
+
+public class Test01 {
+	public static void main(String[] args) {
+		
+	    float  a =1.5f;
+		double b = 1.5;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		float c = 123.123142453463412433452323243657654321456f;
+		double d = 123.12334354657687976543245678654324567654;
+		
+		System.out.println(c);
+		System.out.println(d);
+		
+		
+		System.out.println(10/3);
+		
+	}
+}

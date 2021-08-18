6015836818cb9136927737cf88ddd081e71648da
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-08-18 15:52:30 +0900

    시간 계산 입력 출력

--- day06/src/input/Test05.java
+++ day06/src/input/Test05.java
@@ -6,19 +6,20 @@ public class Test05 {
 		
 		Scanner sc= new Scanner(System.in);
 		
+		System.out.println("시작 시각 (입력 양식 00 00)");
 		int startH = sc.nextInt();
 		int startM = sc.nextInt();
-		System.out.println("시작 시각 : " + startH + " 시간" + startM + " 분");
+		System.out.println("시작 시각 : " +startH + " 시간 "+startM +"분");
 		
-		
+		System.out.println("종료 시각 (입력 양식  00 00)" );
 		int endH = sc.nextInt();
 		int endM = sc.nextInt();
-		System.out.println("종료 시각 : " + endH + " 시간 "+endM +" 분");
-		
+		System.out.println("종료 시각 : " + endH + " 시간 "+endM +"분");
 		sc.close();
 		
+		
 		int pay = 600;
-		int minpay = pay/60;
+		float minpay = pay/60f;
 		
 		int startT = startH*60 + startM;
 		int endT = endH*60 + endM;
@@ -27,9 +28,9 @@ public class Test05 {
 		int totalH = totalT/60;
 		int totalM = totalT%60;
 		
-		System.out.println("이용시간"+totalH+"시간"+totalM+"분");
+		System.out.println("이용시간 : "+totalH+"시간 "+totalM+"분");
 		
-		int totalPay = totalH*pay + totalM*minpay;
+		int totalPay = (int) (totalH*pay + totalM*minpay);
 		
 		System.out.println("계산된 이용 요금 : "+totalPay+"(원)");
 		

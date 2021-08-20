84ba6b0ed1235107b096ab5d8dff24fb79853aba
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-08-20 14:07:09 +0900

    연속 나타내기 디버그 이용

---
+++ day07/src/loop/Test02.java
@@ -0,0 +1,14 @@
+package loop;
+import java.lang.*;
+public class Test02 {
+	public static void main(String[] args) {
+		
+		// 화면에 1부터 10까지 출력
+		// = 1,2,3,4,5,6,7,8,9,10
+		//= 1부터 10까지 1씩 증가하는 데이터
+		
+		for(int i = 1; i <=10 ; i +=1 ) {
+			System.out.println(i);
+		}
+	}
+}

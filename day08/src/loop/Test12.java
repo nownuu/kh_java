87e85a184fbae5b826149ea22803211dbbd13180
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-08-23 09:56:33 +0900

    수정

--- day08/src/loop/Test12.java
+++ day08/src/loop/Test12.java
@@ -3,18 +3,19 @@ package loop;
 public class Test12 {
 	public static void main(String[] args) {
 		
-		int count = 10;
+		int count = 0;
 		int total = 0;
 		
 		for(int i = 1 ; i <=30 ; i ++) {
 			if( i == 1) {
+				count = 10;
 				System.out.println(i+ "일에 하게 되는 푸시업 개수 = "+ count);
 			}
 			else {
 			count +=3;
 			System.out.println(i+ "일에 하게 되는 푸시업 개수 = "+ count);
-			total += i;
 			}
+			total += count;
 		}
 		System.out.println("30일간 하게 되는 푸시업 개수 = " + total);
 	}	

c252280fab9f1e751ff7f00f661e5dc1cba1cc9e
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-08-30 13:52:42 +0900

    메소드

--- day14/src/oop/basic3/Player.java
+++ day14/src/oop/basic3/Player.java
@@ -2,9 +2,28 @@ package oop.basic3;
 
 public class Player {
 
-	String name;
-	String type;
-	int gold;
-	int silver;
-	int bronze;
-}
+	// 맴버변수 - 데이터 저장
+	String name, type;
+	int gold, silver, bronze;
+	
+	//맴버 메소드 - 코드 저장 공간
+	// void 이름(){ 코드 }
+	void init(String name, String type, int gold, int silver, int bronze) {
+		this.name = name;
+		this.type =  type;
+		this.gold = gold; 
+		this.silver = silver;
+		this.bronze = bronze;
+	}
+	
+	void showInfo() {
+		//a, b, c 같은 특정 대상이 아니라 주인공을 의미하는 키워드를 사용 = this
+		System.out.println("선수 정보");
+		System.out.println(this.name);
+		System.out.println(this.type);
+		System.out.println(this.gold);
+		System.out.println(this.silver);
+		System.out.println(this.bronze);
+		}
+	}
+
--- day14/src/oop/basic3/Test02.java
+++ day14/src/oop/basic3/Test02.java
@@ -2,49 +2,18 @@ package oop.basic3;
 
 public class Test02 {
 	public static void main(String[] args) {
+		
 		Player a = new Player();
-		
-		a.name = "김연아";
-		a.type= "피겨스케이팅";
-		a.gold = 2;
-		a.silver = 0;
-		a.bronze = 1;
-		
-		System.out.println(a);
-		System.out.println(a.name);
-		System.out.println(a.type);
-		System.out.println(a.gold);
-		System.out.println(a.silver);
-		System.out.println(a.bronze);
-		
 		Player b = new Player();
-		
-		b.name = "이상화";
-		b.type = "스피드스케이팅";
-		b.gold = 1;
-		b.silver = 2;
-		b.bronze = 1;
-		
-		System.out.println(b);
-		System.out.println(b.name);
-		System.out.println(b.type);
-		System.out.println(b.gold);
-		System.out.println(b.silver);
-		System.out.println(b.bronze);
-		
 		Player c = new Player();
 		
-		c.name = "윤성빈";
-		c.type = "스켈레톤";
-		c.gold = 1;
-		c.silver = 0;
-		c.bronze = 1;
+		a.init("김연아", "피겨스케이팅", 2, 0, 1);
+		b.init("이상화", "스피드스케이팅", 1, 2, 1);
+		c.init("윤성빈", "스켈레톤", 1, 0, 1);
 		
-		System.out.println(c);
-		System.out.println(c.name);
-		System.out.println(c.type);
-		System.out.println(c.gold);
-		System.out.println(c.silver);
-		System.out.println(c.bronze);
+		// this를 사용한 뒤의 출력
+		a.showInfo(); // a를 주인공으로 해서 showInfo 코드 실행
+		b.showInfo(); // b를 주인공으로 해서 showInfo 코드 실행
+		c.showInfo(); // c를 주인공으로 해서 showInfo 코드 실행
 		}
 }

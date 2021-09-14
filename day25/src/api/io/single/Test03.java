c43a0959ed4a836c45709fd26e722af0d1b4fb4d
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-09-14 18:00:00 +0900

    과제

---
+++ day25/src/api/io/single/Test03.java
@@ -0,0 +1,14 @@
+package api.io.single;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Test03 {
+	public static void main(String[] args) throws IOException{
+		
+		File origin = new File("sample", "origin.txt");
+		origin.createNewFile();
+		
+		
+	}
+}

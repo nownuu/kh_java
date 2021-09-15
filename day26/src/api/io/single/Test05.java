79e18389ad087908d7ff46e204d7bd24bda870ea
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-09-15 13:39:38 +0900

    입출력 (카피)

---
+++ day26/src/api/io/single/Test05.java
@@ -0,0 +1,31 @@
+package api.io.single;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class Test05 {
+	public static void main(String[] args) throws IOException{
+		//버퍼를 사용하여 한 번에 여러 개의 바이트 읽기
+		// = 파일마다 크기 수가 다르다
+		// = buffer를 만들때 일정 크기를 지정해서 만들어야한다.
+		// = 적당한 크기가 얼마인지에 대해서 이해할 필요가 있다.
+		
+		File target = new File("sample", "single.txt");
+		InputStream in = new FileInputStream(target);
+		
+		byte[] buffer = new byte[5];
+		
+		int n = in.read(buffer);
+		System.out.println("n = "+ n);
+		System.out.println("buffer = "+Arrays.toString(buffer));
+		
+		in.close();
+		
+		
+		//int data = in.read();
+		// System.out.println("data = "+data);
+	}
+}

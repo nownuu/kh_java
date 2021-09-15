58c2b80fdddfc6dac24b838af5929303c1ea05ac
sohyeon <sohyeon@LAPTOP-7PPCLDC0>
2021-09-15 10:04:23 +0900

    origin 파일에서 copy 복사본 만들기

--- day25/src/api/io/single/Test03_1.java
+++ day25/src/api/io/single/Test03_1.java
@@ -6,7 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class Test03 {
+public class Test03_1 {
 	public static void main(String[] args) throws IOException{
 		
 		//파일 생성
@@ -21,10 +21,21 @@ public class Test03 {
 		OutputStream out =  new FileOutputStream(copy);
 		FileInputStream in = new FileInputStream(origin);
 		
+		// 진행상황표시
+		// = 원본 파일의 byte 크기
+		// = 복사 완료한 byte 크기
 		int data = 0;
+		long total = origin.length();
+		long count = 0L;
 		
 		while((data = in.read()) != -1) {
             out.write(data);
+            
+            out.write(data);
+            count++;
+            
+            float percent = count * 100f / total;
+            System.out.println("총" +total +"개 중 "+count+"byte 복사 완료");
         }
 		
         //종료

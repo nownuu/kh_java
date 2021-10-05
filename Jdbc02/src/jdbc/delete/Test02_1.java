c188948dfeba95f977665017dde93ac1c0c6c741
nownuu <nownuu98@naver.com>
2021-10-05 10:13:59 +0900

    오라클 delete + executeUpdate()

--- Jdbc02/src/jdbc/delete/Test02_1.java
+++ Jdbc02/src/jdbc/delete/Test02_1.java
@@ -1,16 +1,13 @@
-
 package jdbc.delete;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
-public class Test01 {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+public class Test02_1 {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String user = "kh";
 		String password = "kh";
@@ -21,36 +18,28 @@ public class Test01 {
 		System.out.println("Login");
 		System.out.println("===================");
 		
-		// product 상품 번호 제거
-		String sql =("delete from product  where no = ? ");
+		// 아이디와 비밀번호를 받아 일치하는 회원정보 삭제
+		String sql =("delete from member where member_id = ? and member_pw = ?");
 		PreparedStatement ps = con.prepareStatement(sql); 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("삭제할 번호를 입력하십시오."); 
-		int del_no = sc.nextInt();
+		System.out.println("삭제할 아이디를 입력하십시오."); 
+		String del_id = sc.next();
+		System.out.println("삭제할 비밀번호를 입력하십시오."); 
+		String del_pw = sc.next();
 		sc.close();
 		 
-		ps.setInt(1,del_no);
+		ps.setString(1,del_id);
+		ps.setString(2, del_pw);
 		
+//		ps.execute();
 		int result = ps.executeUpdate();
-		if(result == 0) {
-			System.out.println("없는 번호입니다.");
-		}
-		else {
+		if(result  > 0) {
 			System.out.println("삭제 성공");
 		}
-		
-		ps.execute();
+		else {
+			System.out.println("탈퇴가 완료되었습니다.");
+		}
 		con.close();
 		System.out.println("Finish");
-		
-		ResultSet rs = null;
-		Statement st = null;
-		
-		rs = st.executeQuery("select*from product");
-		if(rs.next()) {
-			String name = rs.getString("name");
-		} else {
-			
-		}
 	}
 }

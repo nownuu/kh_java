6365961d2f9fa9c2696ac92d9755fc303f563ff4
nownuu <nownuu98@naver.com>
2021-10-07 13:35:09 +0900

    update 이동

--- Jdbc02/src/jdbc/update/Test06.java
+++ Jdbc02/src/jdbc/update/Test06.java
@@ -1,27 +1,22 @@
-package jdbc.delete;
+package jdbc.update;
 
 import java.util.Scanner;
 
 import jdbc.beans.ProductDao;
 import jdbc.beans.ProductDto;
 
-
-public class Test05 {
+public class Test06 {
 	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("변경하려는 제품의 정보를 입력하십시오");
 		ProductDto productDto = new ProductDto();
 		System.out.print("상품 번호 = ");
 		productDto.setNo(sc.nextInt());
-		productDto.setName("상품명 = "+sc.nextLine());
-		productDto.setType("상품 타입 = " +sc.nextLine());
 		System.out.print("상품 가격 = ");
 		productDto.setPrice(sc.nextInt());
-		productDto.setMade("제조일자 = " +sc.nextLine());
-		productDto.setExpire("유통기한 = " +sc.nextLine());
 		
 		ProductDao productDao = new ProductDao();
-		boolean isSuccess = productDao.update(productDto);
+		boolean isSuccess = productDao.updatePrice(productDto);
 		
 		if(isSuccess) {
 			System.out.println("변경이 완료되었습니다.");
--- Jdbc02/src/jdbc/update/Test07.java
+++ Jdbc02/src/jdbc/update/Test07.java
@@ -1,4 +1,4 @@
-package jdbc.delete;
+package jdbc.update;
 
 import java.util.Scanner;
 
